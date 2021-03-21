6d43a628720c45e1553f65da9a22735523c296ef
Miguel-Barros <miguelnatan2@gmail.com>
2021-03-22 05:33:32 +0900

    Finalização da fase 2 - Calculadora

--- www/components/action.js
+++ www/components/action.js
@@ -1,39 +1,93 @@
+// Const é um tipo de variavel fixa.
+// [{é um array},]
 const types = [
-  {
-    value: "somar",
-    signal: "+",
-  },
-  {
-    value: "dividir",
-    signal: "/",
-  },
-  {
-    value: "multiplicar",
-    signal: "x",
-  },
-  {
-    value: "subtrair",
-    signal: "-",
-  },
-  {
-    value: "resultado",
-    signal: "=",
-  },
+  { value: "0" },
+  { value: "1" },
+  { value: "2" },
+  { value: "3" },
+  { value: "4" },
+  { value: "5" },
+  { value: "6" },
+  { value: "7" },
+  { value: "8" },
+  { value: "9" },
+  { value: "soma", signal: "+" },
+  { value: "dividir", signal: "/" },
+  { value: "multiplicar", signal: "x" },
+  { value: "subtrair", signal: "-" },
+  { value: "virgula", signal: "," },
+  { value: "resultado", signal: "=" },
+  { value: "backspace", signal: "⌫" },
+  { value: "c", signal: "C" },
 ]
 
+// let é um tipo de variavel flexivel (pode ser alterada pelo decorrer do codigo)
+let accumulatedValue = "";
+let operator = "";
+let value = "";
+let currentNumber = "";
+let calc = true;
 
-for (let i = 0; i <= 9; i++) {
-  $(document).on("click", "#" + i, function () {
-    const newI = [...i];
-    $("#painel").val(newI)
-    types.map((result) => {
-      $(document).on("click", "#" + result.value, function () {
-        if (result.signal == "-") {
-          // let result = [...result, i];
-          // console.log(result)
+// converte todo o valor que é pego do display em um valor calculavel (number)
+const convertToCalculable = (display) => {
+  const calculable = display.replaceAll("x", "*").replaceAll(",", ".");
+  return calculable;
+}
+
+// forEach é o mesmo que um for comum, muda apenas o fato de ser mais simples utiliza-lo
+types.forEach((t) => {
+  // verifica qual botão do Array esta sendo clicado e retorna o mesmo.
+  $(document).on("click", "#" + t.value, function () {
+
+    if (t.signal === "C") {
+      currentNumber = "";
+      accumulatedValue = "";
+      operator = "";
+      value = "";
+    } else if (t.signal == "=" && (!accumulatedValue)) {
+        accumulatedValue = "";
+    } else if (['-', 'x', '+', '/', '='].includes(t.signal)) {
+      calc = true;
+     if (t.signal != "=") {
+        calc = false;
+        operator = t.signal;
+        
+        if(accumulatedValue && operator && !value) {
+          accumulatedValue = String(eval(convertToCalculable(accumulatedValue + operator + currentNumber)));
+          currentNumber = accumulatedValue;
+          
+        } else {
+          accumulatedValue = currentNumber;
         }
-        $("#painel").val(result.signal)
-      });
-    })
-  })
-};
+      } else if(value != '') {
+         accumulatedValue = currentNumber; 
+      }
+
+        if (operator) {
+          if (calc && value === '') {
+            value = currentNumber;
+          }
+
+          if(calc && value === '')currentNumber = "";
+        }
+
+
+      if(calc) currentNumber = String(eval(convertToCalculable(accumulatedValue + operator + value)));
+
+    } else if (t.signal == "⌫") {
+      currentNumber = currentNumber.slice(0, -1);
+    } else {
+      if(!calc) {
+        currentNumber = "";
+        calc = true;
+      }
+      currentNumber += (t.signal || t.value);
+    }
+
+// exibe no painel secundario os valores anteriores juntos
+    $("#calc").val(accumulatedValue + operator + value + (!accumulatedValue || operator ? "" : "="));
+    
+    // exibe inicialmente no painel principal o valor 0 
+    $("#painel").val(currentNumber || "0")
+  });
+})
